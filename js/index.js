a1d4da8549dfe9bb78a3af5261b1f4b6bdc49f08
zaballuko <zaballuko@gmail.com>
2018-10-23 17:31:04 +0200

    contact

--- js/index.js
+++ js/index.js
@@ -30,3 +30,26 @@ function changeImage(){
     setTimeout("changeImage()",time);
 }
 window.onload =changeImage;
+
+$(document).ready(function() {
+    $('#contact-form').submit(function(e) {
+      var name    = document.getElementById('inputName')
+      var email   = document.getElementById('inputEmail')
+      var message = document.getElementById('inputMessage')
+  
+      if (!name.value || !email.value || !message.value) {
+        alertify.error("Try again");
+        return false;
+      } else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/jon',
+          data: $('#contact-form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        alertify.success("Message sent");
+      }
+    });
+  });
